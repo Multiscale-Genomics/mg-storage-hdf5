f2455cf3c0922016b7b33e90c81ee3c3336862e2
markmcdowall <mark.mcdowall@gmail.com>
2016-06-23 14:32:21 +0100

    Changes to handle the retrieval of data from chromosomes based on a single array

--- GetRange.py
+++ GetRange.py
@@ -9,8 +9,8 @@ import argparse
 parser = argparse.ArgumentParser(description="Load adjacency list into HDF5 file")
 parser.add_argument("--hdf5", help="HDF5 file name")
 parser.add_argument("--dataset", help="Name of the dataset in the HDF5 file")
-parser.add_argument("--chrLen", help="Chromosome length (bp)")
 parser.add_argument("--binSize", help="Size of the bins (bp)")
+parser.add_argument("--chr", help="Chromosome")
 parser.add_argument("--start", help="Chromosome Start (bp)")
 parser.add_argument("--end", help="Chromosome End (bp)")
 
@@ -18,19 +18,39 @@ parser.add_argument("--end", help="Chromosome End (bp)")
 args = parser.parse_args()
 hdf5 = args.hdf5
 dataset = args.dataset
-chrLen = args.chrLen
 binSize = args.binSize
+chromo = args.chr
 start = float(args.start)
 end = args.end
+
+# Load the chromosome sizes for the whole genome
+chrSizes = open("chrom.size", "r")
+chr_param_list = []
+chr_param = {}
+genomeLen = 0
+for line in chrSizes:
+  line = line.rstrip()
+  line = line.split("\t")
+  
+  c = line[0]
+  l = int(line[1])
+  
+  genomeLen += l
+  
+  chr_param_list.append([c, l, genomeLen])
+  chr_param[c] = [c, l, genomeLen]
+chrSizes.close()
 
 # Defines columns to get extracted from the array
 x = int(np.floor(float(start)/float(binSize)))
 y = int(np.ceil(float(end)/float(binSize)))
 
+xy_offset = chr_param[c][2]-chr_param[c][1]
+
 # Open the hdf5 file
 f = h5py.File(hdf5, "r")
-dset = f[dataset] 
-print dset[x:y,:]
+dset = f[dataset]
+print dset[(x+xy_offset):(y+xy_offset),:]
 
 f.close()
 
